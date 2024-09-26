a1fa316e189abe6cccb14bc83f6651a02792007d
Muskan Srivastav <148577000+muskan171105@users.noreply.github.com>
2024-09-26 11:41:59 +0530

    Delete useroutes.js

--- useroutes.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-const express = require('express');
-const { createUser, getUser } = require('../controllers/userController');
-const router = express.Router();
-
-router.post('/register', createUser);
-router.get('/:walletAddress', getUser);
-
-module.exports = router;
